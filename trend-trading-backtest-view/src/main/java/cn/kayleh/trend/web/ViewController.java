a82420a26f49030508c980d6a9cb998843dc5110
Kayleh <yao924934908@yeah.net>
2020-09-07 23:00:40 +0800

    SpringCloud config

--- trend-trading-backtest-view/src/main/java/cn/kayleh/trend/web/ViewController.java
+++ trend-trading-backtest-view/src/main/java/cn/kayleh/trend/web/ViewController.java
@@ -1,6 +1,9 @@
 package cn.kayleh.trend.web;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
@@ -8,9 +11,14 @@ import org.springframework.web.bind.annotation.GetMapping;
  * @Date: 2020/9/3 8:10
  */
 @Controller
+@RefreshScope//表示允许刷新
 public class ViewController {
+    @Value("${version}")
+    String version;
+
     @GetMapping("/")
-    public String view() throws Exception {
+    public String view(Model model) throws Exception {
+        model.addAttribute("version", version);
         return "view";
     }
 }
